5d10f337df5d9d8497410085c74b2870a4cc024d
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-03-18 21:18:18 -0600

    added starter code

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const message = [ 'c', 'a', 'k', 'e', ' ',
+    'p', 'o', 'u', 'n', 'd', ' ',
+    's', 't', 'e', 'a', 'l' ];
+
+reverseWords(message);
+
+console.log(message.join(''));
+// Prints: 'steal pound cake'
